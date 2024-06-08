c30d7754fcb73d983abd86ab007138b881c11d70
aronaldo9 <aaronpesqueirafarina@gmail.com>
2024-06-08 11:35:55 +0200

    añadida variable env railway

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,12 @@
 export const ENV = {
-  SERVER_HOST: process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337",
+  SERVER_HOST:
+    process.env.NEXT_PUBLIC_STRAPI_RAILWAY_URL ||
+    process.env.NEXT_PUBLIC_STRAPI_URL ||
+    "http://localhost:1337",
   API_URL: `${
-    process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337"
+    process.env.NEXT_PUBLIC_STRAPI_RAILWAY_URL ||
+    process.env.NEXT_PUBLIC_STRAPI_URL ||
+    "http://localhost:1337"
   }/api`,
   ENDPOINTS: {
     AUTH: {
